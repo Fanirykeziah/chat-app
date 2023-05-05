d947cadb87050650035aa3cd3467a50ad3f3314b
FaniryKeziah <hei.keziah.2@gmail.com>
2023-05-05 12:10:25 +0300

    feat: add LocalStorage

--- src/Components/Accueil.tsx
+++ src/Components/Accueil.tsx
@@ -1,10 +1,24 @@
+import { useRouter } from "next/router"
+import { useEffect } from "react";
+
 export default function Accueil() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const username = localStorage.getItem('username');
+    const password = localStorage.getItem('password'); 
+
+    if(!username && !password){
+      router.push('/signup');
+    }
+  }, [])
+
     return (
         <>
            <div className="container">
               <div className="global"></div>
               <div className="chat">
-                <h1>Hello everyone , you're authenticated</h1>
+                <h1>Hello , you're authenticated</h1>
               </div>
            </div>
         </>
--- src/Components/SignUp.tsx
+++ src/Components/SignUp.tsx
@@ -1,12 +1,38 @@
+import { useRouter } from "next/router";
+import { useState } from "react"
+
 export default function SignUp() {
+  const router = useRouter();
+  const[username , setUsername] = useState("");
+  const[password , setPassword] = useState("");
+  
+  const handleSubmit = () => {
+      localStorage.setItem('username',username); 
+      localStorage.setItem('password',password);
+
+    router.push('/');
+  }
+
     return ( 
         <>
            <div className="form-container">
               <h1>Sign Up</h1>
               <div className="form">
-                <input type="email" placeholder="Email" className="input"/>
-                <input type="password" placeholder="Password" className="input"/>
-                <input type="button" value="Save" className="btn" />
+                <input type="text" 
+                placeholder="Username" 
+                className="input" 
+                value={username} 
+                onChange={(e) => setUsername(e.target.value)}/>
+
+                <input type="password" 
+                placeholder="Password" 
+                className="input" 
+                value={password} onChange={(e) => setPassword(e.target.value)}/>
+
+                <input type="button" 
+                value="Save" 
+                className="btn" 
+                onClick={handleSubmit}/>
               </div>
            </div>
         </>
