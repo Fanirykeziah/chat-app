c95a71312a76db9f5bfcc71648516018669f2adc
FaniryKeziah <hei.keziah.2@gmail.com>
2023-05-05 11:30:58 +0300

    feat: add accueil page

---
+++ src/Components/Accueil.tsx
@@ -0,0 +1,12 @@
+export default function Accueil() {
+    return (
+        <>
+           <div className="container">
+              <div className="global"></div>
+              <div className="chat">
+                <h1>Hello everyone , you're authenticated</h1>
+              </div>
+           </div>
+        </>
+    )
+}
