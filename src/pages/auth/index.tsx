88869abdba9080df6aeca56b73506d62407a1e9b
FaniryKeziah <hei.keziah.2@gmail.com>
2023-05-14 18:44:21 +0300

    feat: add pages for authentification

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,12 @@
+import LoginPage from "@/Components/Auth/Login"
+import HeaderTitle from "@/ui/Header/Header"
+
+const Login = () => {
+    return (
+        <HeaderTitle title="Log in">
+            <LoginPage/>
+        </HeaderTitle>
+    )
+}
+
+export default Login; 
---
+++ src/pages/auth/signup.tsx
@@ -0,0 +1,12 @@
+import SignUp from "@/Components/Auth/SignUp";
+import HeaderTitle from "@/ui/Header/Header";
+
+export default function Sign() {
+    return (
+        <HeaderTitle title="Sign Up">
+            <SignUp/>
+        </HeaderTitle>  
+            
+        
+    )
+}
