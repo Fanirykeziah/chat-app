b00758586671e2245fa193c0edb45e9828d020d7
FaniryKeziah <hei.keziah.2@gmail.com>
2023-05-25 11:08:19 +0300

    fix: update Login page

--- src/Components/Auth/Login.tsx
+++ src/Components/Auth/Login.tsx
@@ -1,52 +1,55 @@
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useState } from "react";
-
-interface tokenReponse {
-    token: string 
-}
+import { useCookies } from 'react-cookie';
+import { BASE_URL } from "../utils/baseUrl";
 
 function LoginPage() {
     const [email , setEmail] = useState(''); 
     const [password , setPassword] = useState('');
-    const [error , setError] = useState(''); 
-    const router = useRouter(); 
+    const [error , setError] = useState('');     
+    const [cookies, setCookie] = useCookies(['token']);
+    const router = useRouter();
 
-    const handleLogin =async (e: React.FormEvent) => {
+
+  const handleLogin =async (e: React.FormEvent) => {
         e.preventDefault();
 
     try {
-      const response = await axios.post<tokenReponse>('http://localhost:8080/users/login', { email, password });
-      console.log(response);
-      
+      const response = await axios.post(BASE_URL+'users/login', { email, password });        
+      const token = response.data.user.token; 
+      const options = {
+        path: '/',
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000) // expires in 24 hours
+      };  
+      setCookie("token", token, options); 
+
       setEmail('');
       setPassword('');
       setError('');
 
-      router.push('/');
+    router.push('/channel');
     } catch (error) {
-      setError('Identifiants invalides');
+      setError('Invalid identifiers');
     }
     }
 
-
-    return (
-        <>
-           <form onSubmit={handleLogin}>
-              <input type="email" 
-               placeholder="Email"
-               value={email}
-               onChange={(e) => setEmail(e.target.value)}
-              />
-              <input type="password"
-               placeholder="Password"
-               value={password}
-               onChange={(e) => setPassword(e.target.value)}
-               />
-               {error && <div>{error}</div>}
-              <button type="submit">Log in</button> 
-           </form>
-        </>
+  return (
+    <>
+      <form onSubmit={handleLogin}>
+        <input type="email" 
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}/>
+        <input type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}/>
+        <button type="submit">Sign in</button> 
+        {error && <div>{error}</div>}
+      </form>
+      <p>If you don't have account,<button onClick={() => (router.push("/sign-up"))}>Sign-up</button>, please!</p>
+    </>
     )
 }
 
