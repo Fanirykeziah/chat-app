7369e11f6eacfe20184de169bfee98b833166da7
FaniryKeziah <hei.keziah.2@gmail.com>
2023-06-01 08:31:39 +0300

    fix: update channel Id pages

--- src/pages/channel/[channelId].tsx
+++ src/pages/channel/[channelId].tsx
@@ -1,9 +1,12 @@
 import ChannelId from "@/Components/Channel/ChannelId";
+import ChatChannelId from "@/Components/Chat/ChannelChat";
+import { useCookies } from "react-cookie";
 
 export default function getChannelId() {
+
     return (
         <>
-          <ChannelId {}/>
+          <ChatChannelId/>
         </>
     )
 }
